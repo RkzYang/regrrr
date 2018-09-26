f799f7e693c090adc9899a0f6ad5d638cfd3da29
Ray K Yang <ray.cn.us@gmail.com>
2018-09-25 22:20:53 -0700

    update

--- R/Fast.Check.R
+++ R/Fast.Check.R
@@ -1,6 +1,6 @@
 # quickly check the vifs of a data.frame, which can be from a model #
 reg.Vif <- function(model_df){
-  usdm::vif(model_df[,which(sapply(model_df, class) %in% c("numeric","integer","AsIs"))])
+  usdm::vif(model_df[,which(sapply(model_df, class) %in% c("numeric","integer","AsIs"))][,-1])
 }
 
 # quickly check the complete correlaiton matrix, or the correlation between a variable X and all other vars #
@@ -15,4 +15,3 @@ reg.Cor <- function(model_df, var_name_select = NULL, d = 3){
   }
   return(result)
 }
-
