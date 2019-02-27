03d9fe29d2f7708b9ccd9693968c24329394a187
Ray K Yang <ray.cn.us@gmail.com>
2019-02-27 14:11:53 -0800

    update

--- R/regrrr.R
+++ R/regrrr.R
@@ -7,20 +7,3 @@
 #' @docType package
 #' @name regrrr
 "_PACKAGE"
-
-#' mtcars
-#' @title
-#' mtcars
-#'
-#' @description
-#' Motor Trend Car Road Tests
-#'
-#' @docType data
-#' @name mtcars
-#' @usage data(mtcars)
-#'
-#'
-#' @keywords datasets
-#' @source Henderson and Velleman (1981), Building multiple regression models interactively. Biometrics, 37, 391–411.
-#'
-"mtcars"
